4fadf1c802d2903a490d7f75b1b779d8e808fafe
Breanna Higgins <bbhiggins520@outlook.com>
2017-04-03 21:21:38 -0400

    Defined motorTest function

--- MSP430/src/main.c
+++ MSP430/src/main.c
@@ -10,6 +10,19 @@
 #define TIMER_PERIOD 511
 #define DUTY_CYCLE  350
 
+/**
+ * @fn
+ * @brief Drive all motors CW
+ */
+void motorTest();
+void motorTest(){
+    motorsInit();
+
+    setFrontMotorA(CW);
+    setFrontMotorB(CW);
+    setBackMotorA(CW);
+    setBackMotorB(CW);
+}
 
 void main(void)
 {
@@ -36,36 +49,18 @@ void main(void)
     GPIO_setOutputHighOnPin(GPIO_PORT_P8, GPIO_PIN0|GPIO_PIN1);
 
 //    //P1.3 as PWM output
-//	GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN3);
+//  GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN3);
 //
-//	//Generate PWM on A0.2 - Timer runs in Up mode
-//	Timer_A_outputPWMParam param = {0};
-//	param.clockSource = TIMER_A_CLOCKSOURCE_ACLK;//TIMER_A_CLOCKSOURCE_SMCLK;
-//	param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
-//	param.timerPeriod = TIMER_PERIOD;
-//	param.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_2;
-//	param.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
-//	param.dutyCycle = DUTY_CYCLE;
-//	Timer_A_outputPWM(TIMER_A0_BASE, &param);
+//  //Generate PWM on A0.2 - Timer runs in Up mode
+//  Timer_A_outputPWMParam param = {0};
+//  param.clockSource = TIMER_A_CLOCKSOURCE_ACLK;//TIMER_A_CLOCKSOURCE_SMCLK;
+//  param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+//  param.timerPeriod = TIMER_PERIOD;
+//  param.compareRegister = TIMER_A_CAPTURECOMPARE_REGISTER_2;
+//  param.compareOutputMode = TIMER_A_OUTPUTMODE_RESET_SET;
+//  param.dutyCycle = DUTY_CYCLE;
+//  Timer_A_outputPWM(TIMER_A0_BASE, &param);
 
-    //PA.x output
-    GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN5|GPIO_PIN6);
-
-    //Set all PA pins HI
-    GPIO_setOutputHighOnPin(GPIO_PORT_P2, GPIO_PIN5);
-    GPIO_setOutputLowOnPin(GPIO_PORT_P2, GPIO_PIN6);
+    motorTest();
 
 }
-
-/**
- * @fn
- * @brief Drive all motors CW
- */
-void motorTest();
-void motorTest(){
-    //init (f/B)inx
-
-    motorsInit();
-}
-
-
